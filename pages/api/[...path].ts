1cd22924ecca9faf95c449eb563891c2f3954710
cuongnc <codondemen1102@gmail.com>
2022-07-25 23:10:31 +0700

    Fixed "API resolved but no response..."

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -14,14 +14,18 @@ export const config = {
 const proxy = httpProxy.createProxyServer()
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  // dont send cookies to API server
-  req.headers.cookie = ''
-  // /api/students/
-  // https://js-post-api.herokuapp.com/api/students/
-  proxy.web(req, res, {
-    target: process.env.API_URL,
-    changeOrigin: true,
-    selfHandleResponse: false,
+  return new Promise((resolve) => {
+    // dont send cookies to API server
+    req.headers.cookie = ''
+    // /api/students/
+    // https://js-post-api.herokuapp.com/api/students/
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false,
+    })
+    proxy.once('proxyRes', () => {
+      resolve(true)
+    })
   })
-  // res.status(200).json({ name: 'Cuong Nguyen' })
 }

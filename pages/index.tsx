613fc449a75fe378292de204efd8c8c5e2647beb
cuongnc <codondemen1102@gmail.com>
2022-08-08 02:57:29 +0700

    SEO for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { SEO } from '@/components/common/seo'
 import { FreaturedWorks, HeroSection, RecentPosts } from '@/components/home'
 import { MainLayout } from '@/components/layout'
 import { NextPageWithLayout } from '@/models'
@@ -6,6 +7,15 @@ import { Box } from '@mui/material'
 const Home: NextPageWithLayout = () => {
   return (
     <Box>
+      <SEO
+        data={{
+          title: 'Learn Nextjs | Cuongnc',
+          description:
+            'Step by step tutorial to build a full CRUD website using Nextjs for beginners ',
+          url: 'https://cuongnc-learn-nextjs.vercel.app/',
+          thumbnailUrl: 'https://miro.medium.com/max/700/1*htbUdWgFQ3a94PMEvBr_hQ.png',
+        }}
+      />
       <HeroSection />
       <RecentPosts />
       <FreaturedWorks />
